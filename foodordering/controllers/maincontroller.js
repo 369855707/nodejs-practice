f399b7b2bf56099d051eea970f5080645a6793a6
369855707 <369855707@qq.com>
2020-02-12 09:47:28 +0800

    master | add mongodb insert function

--- foodordering/controllers/maincontroller.js
+++ foodordering/controllers/maincontroller.js
@@ -3,33 +3,43 @@ const formidable = require('formidable');
 const file = require('../models/file');
 
 
-exports.saveOrder = (req,res) => {
+exports.saveOrder = (req, res) => {
     var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields) {
+    form.parse(req, function (err, fields) {
         var food = fields.food;
         var phone = fields.phone;
-        file.save(phone, food, (err) => {
-            if(err) {
-                res.send("-1")
+        //1.save into txt file
+        // file.save(phone, food, (err) => {
+        //     if (err) {
+        //         res.send("-1")
+        //     } else {
+        //         res.send("1")
+        //     }
+        // });
+
+        //2.save into mongodb
+        file.mongoSave(phone, food, (err) => {
+            if (err) {
+                res.send("-1");
             } else {
-                res.send("1")
+                res.send("1");
             }
-        });
+        })
     })
 }
 
-exports.showAllOrders = (req,res) => {
-    file.readAllFiles(function(files){
-        res.render("allorders.ejs",{
-            orders : files
+exports.showAllOrders = (req, res) => {
+    file.readAllFiles(function (files) {
+        res.render("allorders.ejs", {
+            orders: files
         })
     })
 }
 
 exports.oneorder = (req, res) => {
     file.readonefile(req.params.phone, (data) => {
-        res.render('oneorder.ejs',{
-            food : data
+        res.render('oneorder.ejs', {
+            food: data
         })
     })
 }
--- foodordering/models/file.js
+++ foodordering/models/file.js
@@ -1,3 +1,4 @@
+const MongoClient = require('mongodb').MongoClient;
 const fs = require("fs");
 var baseurl = 'data/';
 
@@ -6,13 +7,44 @@ exports.save = (phone, food, callback) => {
 }
 
 exports.readAllFiles = (callback) => {
-    fs.readdir(baseurl, function(err, files) {
+    fs.readdir(baseurl, function (err, files) {
         callback(files);
     })
 }
- exports.readonefile = (phone, callback) => {
-    fs.readFile(baseurl + phone , (err, data) => {
+
+exports.readonefile = (phone, callback) => {
+    fs.readFile(baseurl + phone, (err, data) => {
         if (err) throw err;
         callback(data);
     })
+}
+
+exports.mongoSave = (phone, food, callback) => {
+
+    // Connection URL
+    const dburl = 'mongodb://localhost:27017';
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(dburl, function (err, client) {
+        if (err) {
+            console.log("mongod please.");
+            return;
+        }
+        console.log("connected successful");
+
+        const col = client.db("food").collection('orders');
+
+        col.insertOne({
+            "food": food,
+            "phone": phone
+        }, (err, r) => {
+            if (err) {
+                console.log("insert failed");
+                return;
+            }
+            console.log("insert successfully " + r.insertedCount + " row");
+
+            client.close();
+        })
+    });
 }
